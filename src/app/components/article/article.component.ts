a0fbfadeeb0a9695fce851eadd9e8a7f30e53411
Vitor Fel <vitorfel2000@gmail.com>
2023-12-09 17:20:26 -0300

    :recycle: Article component assigning input

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+
+  @Input() title = 'Lorem, ipsum dolor.';
+  @Input() description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ea quo unde vel adipisci blanditiis voluptates eum. Nam, cum minima?';
+  @Input() date = new Date();
+  @Input() author = 'Lorem ipsum';
 
 }
